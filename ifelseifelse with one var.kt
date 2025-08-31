2e7fe6648edf05b1cb969066d7963b211366ebcf
Geetha-S-03 <168105598+Geetha-S-03@users.noreply.github.com>
2025-08-31 19:10:02 +0530

    Create ifelseifelse with one var.kt

---
+++ ifelseifelse with one var.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val x="Red"
+    val message=if(x=="Red")"Stop"
+    else if(x=="Yellow")"Ready"
+    else if(x=="Green")"Go"
+    else "Invalid input"
+    
+    println(message)
+  
+}
