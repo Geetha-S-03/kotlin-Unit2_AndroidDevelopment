ca71ef3ed0ba0854e9df70f56ffdc2f531eebc46
Geetha-S-03 <168105598+Geetha-S-03@users.noreply.github.com>
2025-08-31 18:27:13 +0530

    Create IfElseifElse.kt

---
+++ IfElseifElse.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val trafficLight="Yellow"
+    if(trafficLight=="Red"){
+        println("Stop!")
+    }
+    else if(trafficLight=="Yellow"){
+        println("Ready!")
+    }
+    else if(trafficLight=="Green"){
+        println("Go")
+    }
+    
+    else{
+        println("Invalid")
+    }
+}
